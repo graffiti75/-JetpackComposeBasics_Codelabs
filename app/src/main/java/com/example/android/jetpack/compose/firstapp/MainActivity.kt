91699d29446526cca3cf996587875488df730e80
Rodrigo Cericatto Konzen <graffiti75@gmail.com>
2023-05-26 18:25:16 -0400

    Finishing touches added

--- app/src/main/java/com/example/android/jetpack/compose/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/android/jetpack/compose/firstapp/MainActivity.kt
@@ -4,8 +4,8 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateContentSize
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -14,8 +14,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.Button
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,6 +33,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -37,7 +44,7 @@ class MainActivity : ComponentActivity() {
 		super.onCreate(savedInstanceState)
 		setContent {
 			FirstAppTheme {
-				MyApp()
+				MyApp(modifier = Modifier.fillMaxSize())
 			}
 		}
 	}
@@ -45,36 +52,55 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun Greeting(name: String) {
-	var expanded by rememberSaveable { mutableStateOf(false) }
-	val extraPadding by animateDpAsState(
-		if (expanded) 48.dp else 0.dp,
-		animationSpec = spring(
-			dampingRatio = Spring.DampingRatioMediumBouncy,
-			stiffness = Spring.StiffnessLow,
-		)
-	)
-	Surface(
-		color = MaterialTheme.colorScheme.primary,
+	Card(
+		colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.primary),
 		modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
 	) {
-		Row(modifier = Modifier.padding(24.dp)) {
-			Column(modifier = Modifier.weight(1f)
-//				.padding(bottom = extraPadding)
-				.padding(bottom = extraPadding.coerceAtLeast(0.dp))
-			) {
-				Text(text = "Hello, ")
+		CardContent(name)
+	}
+}
+
+@Composable
+private fun CardContent(name: String) {
+	var expanded by remember { mutableStateOf(false) }
+
+	Row(
+		modifier = Modifier
+			.padding(12.dp)
+			.animateContentSize(
+				animationSpec = spring(
+					dampingRatio = Spring.DampingRatioMediumBouncy,
+					stiffness = Spring.StiffnessLow
+				)
+			)
+	) {
+		Column(
+			modifier = Modifier
+				.weight(1f)
+				.padding(12.dp)
+		) {
+			Text(text = "Hello, ")
+			Text(
+				text = name, style = MaterialTheme.typography.headlineMedium.copy(
+					fontWeight = FontWeight.ExtraBold
+				)
+			)
+			if (expanded) {
 				Text(
-					text = name,
-					style = MaterialTheme.typography.headlineMedium.copy(
-						fontWeight = FontWeight.ExtraBold
-					)
+					text = ("Composem ipsum color sit lazy, " +
+							"padding theme elit, sed do bouncy. ").repeat(4),
 				)
 			}
-			ElevatedButton(
-				onClick = { expanded = !expanded }
-			) {
-				Text(if (expanded) "Show less" else "Show more")
-			}
+		}
+		IconButton(onClick = { expanded = !expanded }) {
+			Icon(
+				imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+				contentDescription = if (expanded) {
+					stringResource(R.string.show_less)
+				} else {
+					stringResource(R.string.show_more)
+				}
+			)
 		}
 	}
 }
@@ -82,7 +108,7 @@ private fun Greeting(name: String) {
 @Composable
 fun MyApp(modifier: Modifier = Modifier) {
 	var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
-	Surface(modifier) {
+	Surface(modifier, color = MaterialTheme.colorScheme.background) {
 		if (shouldShowOnboarding) {
 			OnboardingScreen(onContinueClicked = { shouldShowOnboarding = false })
 		} else {
@@ -90,20 +116,6 @@ fun MyApp(modifier: Modifier = Modifier) {
 		}
 	}
 }
-
-/*
-@Composable
-private fun Greetings(
-	modifier: Modifier = Modifier,
-	names: List<String> = listOf("World", "Compose")
-) {
-	Column(modifier = modifier.padding(vertical = 4.dp)) {
-		for (name in names) {
-			Greeting(name = name)
-		}
-	}
-}
-*/
 
 @Composable
 private fun Greetings(
@@ -129,8 +141,7 @@ fun OnboardingScreen(
 	) {
 		Text("Welcome to the Basics Codelab!")
 		Button(
-			modifier = Modifier
-				.padding(vertical = 24.dp),
+			modifier = Modifier.padding(vertical = 24.dp),
 			onClick = onContinueClicked
 		) {
 			Text("Continue")
@@ -143,25 +154,16 @@ fun OnboardingScreen(
 // Previews
 //--------------------------------------------------
 
-@Preview(showBackground = true, widthDp = 320)
-@Composable
-private fun GreetingsPreview() {
-	FirstAppTheme {
-		Greetings()
-	}
-}
-
 @Preview(
 	showBackground = true,
 	widthDp = 320,
 	uiMode = UI_MODE_NIGHT_YES,
 	name = "Dark"
 )
-@Preview(showBackground = true, widthDp = 320)
 @Composable
 fun DefaultPreview() {
 	FirstAppTheme {
-		MyApp()
+		Greetings()
 	}
 }
 
